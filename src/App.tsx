43837a5099ea800c3d3110918d25ba74875edcee
diiyavol <nikitanesterenko88@gmail.com>
2026-09-24 16:26:42 +0300

    solution 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 import './App.scss';
-import {
-  Link,
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
-  HashRouter as Router,
-} from 'react-router-dom';
+import { Link, Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import { HomePage } from './HomePage';
 import { PeoplePage } from './PeoplePage';
 import { PageNotFound } from './PageNotFound';
@@ -24,51 +17,49 @@ export const App = () => {
   };
 
   return (
-    <Router>
-      <div data-cy="app">
-        <nav
-          data-cy="nav"
-          className="navbar is-fixed-top has-shadow"
-          role="navigation"
-          aria-label="main navigation"
-        >
-          <div className="container">
-            <div className="navbar-brand">
-              <Link
-                className={classNames('navbar-item', {
-                  'has-background-grey-lighter': isActive('/'),
-                })}
-                to="/"
-              >
-                Home
-              </Link>
+    <div data-cy="app">
+      <nav
+        data-cy="nav"
+        className="navbar is-fixed-top has-shadow"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <Link
+              className={classNames('navbar-item', {
+                'has-background-grey-lighter': isActive('/'),
+              })}
+              to="/"
+            >
+              Home
+            </Link>
 
-              <Link
-                className={classNames('navbar-item', {
-                  'has-background-grey-lighter': isActive('/people'),
-                })}
-                to="/people"
-              >
-                People
-              </Link>
-            </div>
+            <Link
+              className={classNames('navbar-item', {
+                'has-background-grey-lighter': isActive('/people'),
+              })}
+              to="/people"
+            >
+              People
+            </Link>
           </div>
-        </nav>
+        </div>
+      </nav>
 
-        <main className="section">
-          <div className="container">
-            <Routes>
-              <Route path="home" element={<Navigate to="/" replace />} />
-              <Route path="/" element={<HomePage />} />
-              <Route path="people">
-                <Route index element={<PeoplePage />} />
-                <Route path=":slug" element={<PeoplePage />} />
-              </Route>
-              <Route path="*" element={<PageNotFound />}></Route>
-            </Routes>
-          </div>
-        </main>
-      </div>
-    </Router>
+      <main className="section">
+        <div className="container">
+          <Routes>
+            <Route path="home" element={<Navigate to="/" replace />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="people">
+              <Route index element={<PeoplePage />} />
+              <Route path=":slug" element={<PeoplePage />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </div>
+      </main>
+    </div>
   );
 };
